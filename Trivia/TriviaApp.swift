b7329900a759cc95070d527919df38d003f7484a
jamiecartin <jamie.cartin@icloud.com>
2023-03-29 21:25:43 -0400

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Jamie Cartin on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
