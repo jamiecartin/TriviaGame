c4fa298beb505260e0bf8c89d0fdb0eaa4c5beb9
jamiecartin <jamie.cartin@icloud.com>
2023-04-06 00:02:24 -0400

    add ability to move to next question

--- Trivia/TriviaManager.swift
+++ Trivia/TriviaManager.swift
@@ -6,10 +6,18 @@
 //
 
 import Foundation
+import SwiftUI
 
 class TriviaManager: ObservableObject {
     private(set) var trivia: [Trivia.Result] = []
     @Published private(set) var length = 0
+    @Published private(set) var index = 0
+    @Published private(set) var reachedEnd = false
+    @Published private(set) var answerSelected = false
+    @Published private(set) var question: AttributedString = ""
+    @Published private(set) var answerChoices: [Answer] = []
+    @Published private(set) var progress: CGFloat = 0.00
+    
     
     init() {
         Task.init {
@@ -34,9 +42,30 @@ class TriviaManager: ObservableObject {
             DispatchQueue.main.async {
                 self.trivia = decodedData.results
                 self.length = self.trivia.count
+                self.setQuestion()
             }
         } catch {
             print("Error fetching trivia: \(error)")
+        }
+    }
+    
+    func goToNextQuestion() {
+        if index + 1 < length {
+            index += 1
+            setQuestion()
+        } else {
+            reachedEnd = true
+        }
+    }
+    
+    func setQuestion() {
+        answerSelected = false
+        progress = CGFloat(Double(index + 1) / Double(length) * 350)
+        
+        if index < length {
+            let currentQuestion = trivia[index]
+            question = currentQuestion.formattedQuestion
+            answerChoices = currentQuestion.answers
         }
     }
 }
